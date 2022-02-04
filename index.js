f8a6a1db8d6dac7e55025eec37d9d364c5dd6a46
Brandon Denney <ee.brandon@gmail.com>
2022-02-04 15:52:56 -0500

    Added solutions.

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 // This is just allowing us to use the dataset from the other file. You can ignore this line.
 const UFO_SIGHTINGS = require("./nyc-ufo-sightings.json");
+
+function calculateMinutes(seconds) {
+    return Math.floor(seconds / 60);
+}
+
+function getRandomSighting() {
+    return UFO_SIGHTINGS[Math.floor(Math.random() * UFO_SIGHTINGS.length)];
+}
+
+function shapeTypes() {
+    return UFO_SIGHTINGS.reduce((result, sighting) => {
+        if (!result.includes(sighting.shape)) {
+            result.push(sighting.shape);
+        }
+        return result;
+    }, []);
+}
+
+function calculateViewDuration(ufoSighting) {
+    let duration = ufoSighting.duration;
+    if (duration < 120) {
+        return "Short";
+    } else if (duration < 240) {
+        return "Average";
+    } else {
+        return "Long"
+    }
+}
+
+function averageSightingDuration() {
+    let sum = UFO_SIGHTINGS.reduce((total, sighting) => {
+        return total + sighting.duration;
+    }, 0);
+
+    return sum / UFO_SIGHTINGS.length;
+}
+
+function findFirstLongSighting() {
+    return UFO_SIGHTINGS.find(sighting => {
+        return sighting.duration > (10 * 60);
+    });
+}
+
+function findShortComments() {
+    const shortSightings = UFO_SIGHTINGS.filter(sighting => {
+        return sighting.duration < 60;
+    });
+
+    return shortSightings.map(sighting => sighting.comments)
+}
